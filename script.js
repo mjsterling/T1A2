a9ea47510b4ca70bef32f73b4d4963b388b20613
Matthew Sterling <mjsterling93@gmail.com>
2021-03-02 00:52:00 +1100

    altered colours. fixed menu overlay.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function toggleDarkMode() {
   let dmb = document.getElementById("darkbutton");
   if (dmtoggle == false) {
     bfc.style.color = "white";
-    bfc.style.background = "#2e2e2e";
+    bfc.style.background = "#0c0c0c";
     dmb.style.filter = "invert(1)";
     dmtoggle = true;   
     }
@@ -21,19 +21,19 @@ function toggleDarkMode() {
 
 function toggleMenu() {
   let bfc = document.getElementById("body-flex-container"),
-      menuoverlay = document.getElementById("menuoverlay"),
+    //   menuoverlay = document.getElementById("menuoverlay"),
       menubtn = document.getElementById("menubutton");
   if (menutoggle == false) {
     bfc.style.height = "64vh";
     bfc.style.top = "24vh";
-    menuoverlay.style.zIndex = "3";
+    // menuoverlay.style.zIndex = "3";
     menubtn.style.filter = "invert(1)";
     menutoggle = true;
   } else {
     bfc.style.height = "88vh";
     bfc.style.top = "10vh";
 
-    menuoverlay.style.zIndex = "-1";
+    // menuoverlay.style.zIndex = "-1";
     menubtn.style.filter = "invert(0)";
     menutoggle = false;
   }
