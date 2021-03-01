f118ba0ce37fa1f5f34b8a9544aabf2935804663
Matthew Sterling <mjsterling93@gmail.com>
2021-03-02 00:19:31 +1100

    major alterations to mobile UI, still no desktop

--- script.js
+++ script.js
@@ -1,92 +1,40 @@
 var menutoggle = false;
+var dmtoggle = false;
 
-function enableDarkMode() {
-    let bfc = document.getElementById("body-flex-container");
-        bfc.style.background = ("#ededed");
-        bfc.style.color = ("#2e2e2e");
-    setTimeout(() => {
-        bfc.style.background = ("#d4d4d4");
-        bfc.style.color = ("#404040");
-    }, 60);
-    setTimeout(() => {
-        bfc.style.background = ("#bdbdbd");
-        bfc.style.color = ("#636363");
-    }, 60);
-    setTimeout(() => {
-        bfc.style.background = ("#9c9c9c");
-        bfc.style.color = ("#9c9c9c");
-    }, 60);
-    setTimeout(() => {
-        bfc.style.background = ("#636363");
-        bfc.style.color = ("#bdbdbd");
-    }, 60);
-    setTimeout(() => {
-        bfc.style.background = ("#404040");
-        bfc.style.color = ("#d4d4d4");
-    }, 60);
-    setTimeout(() => {
-        bfc.style.background = ("#2e2e2e");
-        bfc.style.color = ("#ededed");
-    }, 60);
-    setTimeout(() => {
-        bfc.style.background = ("#0c0c0c");
-        bfc.style.color = ("white");
-    }, 60);
-    document.getElementById("disableDarkButton").style.zIndex = "2";
-    document.getElementById("enableDarkButton").style.zIndex = "1";
-}
+function toggleDarkMode() {
+  let bfc = document.getElementById("body-flex-container");
+  let dmb = document.getElementById("darkbutton");
+  if (dmtoggle == false) {
+    bfc.style.color = "white";
+    bfc.style.background = "#2e2e2e";
+    dmb.style.filter = "invert(1)";
+    dmtoggle = true;   
+    }
 
-function disableDarkMode() {
-    let bfc = document.getElementById("body-flex-container");
-        bfc.style.background = ("#2e2e2e");
-        bfc.style.color = ("#ededed");
-    setTimeout(() => {
-        bfc.style.background = ("#404040");
-        bfc.style.color = ("#d4d4d4");
-    }, 60);
-    setTimeout(() => {
-        bfc.style.background = ("#636363");
-        bfc.style.color = ("#bdbdbd");
-    }, 60);
-    setTimeout(() => {
-        bfc.style.background = ("#9c9c9c");
-        bfc.style.color = ("#9c9c9c");
-    }, 60);
-    setTimeout(() => {
-        bfc.style.background = ("#bdbdbd");
-        bfc.style.color = ("#636363");
-    }, 60);
-    setTimeout(() => {
-        bfc.style.background = ("#d4d4d4");
-        bfc.style.color = ("#404040");
-    }, 60);
-    setTimeout(() => {
-        bfc.style.background = ("#ededed");
-        bfc.style.color = ("#2e2e2e");
-    }, 60);
-    setTimeout(() => {
-        bfc.style.background = ("white");
-        bfc.style.color = ("#0c0c0c");
-    }, 60);
-    document.getElementById("disableDarkButton").style.zIndex = "1";
-    document.getElementById("enableDarkButton").style.zIndex = "2";
+    else {
+    bfc.style.background = "white";
+    bfc.style.color = "#0c0c0c";
+    dmb.style.filter = "invert(0)";
+    dmtoggle = false;   
+    }
 }
 
 function toggleMenu() {
-    let inpage = document.getElementById("inpage");
-    let bfc = document.getElementById("body-flex-container");
+  let bfc = document.getElementById("body-flex-container"),
+      menuoverlay = document.getElementById("menuoverlay"),
+      menubtn = document.getElementById("menubutton");
+  if (menutoggle == false) {
+    bfc.style.height = "64vh";
+    bfc.style.top = "24vh";
+    menuoverlay.style.zIndex = "3";
+    menubtn.style.filter = "invert(1)";
+    menutoggle = true;
+  } else {
+    bfc.style.height = "88vh";
+    bfc.style.top = "10vh";
 
-    if (menutoggle == false) {
-        inpage.style.zIndex = "1";
-        menutoggle = true;
-        bfc.style.top = "18vh";
-        bfc.style.height = "74vh";
-    }
-    
-    else {
-        inpage.style.zIndex = "-1";
-        menutoggle = false;
-        bfc.style.top = "12vh";
-        bfc.style.height = "80vh";
-    }
+    menuoverlay.style.zIndex = "-1";
+    menubtn.style.filter = "invert(0)";
+    menutoggle = false;
+  }
 }
