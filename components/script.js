e156808b9e2f7ffdde265c6094b0a5601259511d
Matthew Sterling <mjsterling93@gmail.com>
2021-03-02 14:59:56 +1100

    major changes, fixed most display issues

--- components/script.js
+++ components/script.js
@@ -3,18 +3,29 @@ var dmtoggle = false;
 
 function toggleDarkMode() {
   let bfc = document.getElementById("body-flex-container");
-  let dmb = document.getElementById("darkbutton");
+  let db = document.getElementById("darkbutton");
+  let bkg = document.getElementById("background");
+  let ppic = document.getElementById("profilepic");
+
   if (dmtoggle == false) {
     bfc.style.color = "white";
     bfc.style.background = "#0c0c0c";
-    dmb.style.filter = "invert(1)";
-    dmtoggle = true;   
+    bfc.style.borderColor = "white";
+    db.style.filter = "invert(1)";
+    bkg.style.filter = "invert(0)";
+    ppic.style.borderColor = "white";
+    ppic.style.background = "white";
+    dmtoggle = true;
     }
 
     else {
     bfc.style.background = "white";
     bfc.style.color = "#0c0c0c";
-    dmb.style.filter = "invert(0)";
+    bfc.style.borderColor = "#33095e";
+    db.style.filter = "invert(0)";
+    bkg.style.filter = "invert(0.8)";
+    ppic.style.borderColor = "#33095e";
+    ppic.style.background = "#33095e";
     dmtoggle = false;   
     }
 }
@@ -23,13 +34,13 @@ function toggleMenu() {
   let bfc = document.getElementById("body-flex-container"),
       menubtn = document.getElementById("menubutton");
   if (menutoggle == false) {
-    bfc.style.height = "64%";
+    bfc.style.height = "66%";
     bfc.style.top = "24%";
     menubtn.style.filter = "invert(1)";
     menutoggle = true;
   } else {
     bfc.style.height = "86%";
-    bfc.style.top = "11%";
+    bfc.style.top = "12%";
 
     menubtn.style.filter = "invert(0)";
     menutoggle = false;
